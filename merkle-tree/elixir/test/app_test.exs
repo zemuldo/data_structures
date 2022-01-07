c3ddc8d2039390e82d684ae0f2ebe2d499733308
Zemuldo <danstan.otieno@gmail.com>
2022-01-07 09:35:16 +0300

    adds verify transaction

--- merkle-tree/elixir/test/app_test.exs
+++ merkle-tree/elixir/test/app_test.exs
@@ -31,4 +31,65 @@ defmodule AppTest do
 
     assert hash == t.hash
   end
+
+  test "Create Merkle Tree" do
+    block = %Block{id: UUID.uuid4()}
+
+    {:ok, datetime, 0} = DateTime.from_iso8601("2022-01-06T00:00:00Z")
+
+    1..10
+    |> Enum.to_list()
+    |> Enum.map(fn n ->
+      App.Block.add_transaction(
+        Transaction.init(n * 4, n * 200, n * 1000, Timex.shift(datetime, hours: n), block),
+        block
+      )
+    end)
+
+    assert 10 == App.Block.count_transactions(block)
+
+    tree = App.Block.get_transaction_hashes(block) |> App.MerkleTree.create()
+
+    assert Enum.at(tree, 0) == [
+             "a8c649cf6a9412d55b5c210a041e3a68da399b557193d96ec5a93b8d11fabcd8"
+           ]
+  end
+
+  test "Very Transaction" do
+    block = %Block{id: UUID.uuid4()}
+
+    {:ok, datetime, 0} = DateTime.from_iso8601("2022-01-06T00:00:00Z")
+
+    1..100
+    |> Enum.to_list()
+    |> Enum.map(fn n ->
+      App.Block.add_transaction(
+        Transaction.init(n * 4, n * 200, n * 1000, Timex.shift(datetime, hours: n), block),
+        block
+      )
+    end)
+
+    tree = App.Block.get_transaction_hashes(block) |> App.MerkleTree.create() |> IO.inspect()
+    n = 9
+
+    root =
+      Block.verify_transaction(
+        Transaction.init(n * 4, n * 200, n * 1000, Timex.shift(datetime, hours: n), block),
+        block,
+        tree
+      )
+
+    assert root == tree |> Enum.at(0) |> Enum.at(0)
+
+    n = 99
+
+    root =
+      Block.verify_transaction(
+        Transaction.init(n * 4, n * 200, n * 1000, Timex.shift(datetime, hours: n), block),
+        block,
+        tree
+      )
+
+    assert root == tree |> Enum.at(0) |> Enum.at(0)
+  end
 end
